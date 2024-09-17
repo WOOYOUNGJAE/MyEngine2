95cbe7b3b4603a6934177f0c2d841967557f7e73
WOOYOUNGJAE <76108701+WOOYOUNGJAE@users.noreply.github.com>
2024-09-17 19:52:51 +0900

    Initial commit

---
+++ App/pch.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "../Engine/pch.h"
+#include "Resource.h"
+
+// Engine Lib
+#pragma comment(lib, "Engine.lib")
---
+++ Engine/Headers/DeviceResources.h
@@ -0,0 +1,10 @@
+#pragma once
+class ENGINE_DLL DeviceResources
+{
+	DECLARE_SINGLETON(DeviceResources);
+public:
+	DeviceResources() = default;
+	~DeviceResources() = default;
+public:
+};
+
